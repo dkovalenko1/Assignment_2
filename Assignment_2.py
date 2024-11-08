221cde8a30419fcb3eaa894616cfb409e187c0a1
dkovalenko1 <dkovalenko1@kse.org.ua>
2024-11-08 13:50:45 +0200

    level 1

---
+++ Assignment_2.py
@@ -0,0 +1,27 @@
+digits = '0123456789ABCDEF'
+
+def decimal_to_binary(number, system=2):
+    final = ''
+    while number > 0:
+        final = digits[number % system] + final
+        number //= system
+    return final
+
+def binary_to_decimal(number, system=2):
+    final = 0
+    for digit in number:
+        final = final * system + digits.index(digit)
+    return final
+
+
+def main():
+    number = input("Enter a number (if decimal - without prefix, if binary - with prefix 0b) : ")
+    while not (number[0:2] == "0b" or number.isdecimal()):
+        number = input("Enter a number (if decimal - without prefix, if binary - with prefix 0b) : ")
+    if number[0:2] == '0b':
+        decimal_from_binary = binary_to_decimal(number[2:])
+        print(f"Your number {number} in decimal - {binary_to_decimal(number[2:])}")
+    else:
+        print(f"Your number {number} in binary - {decimal_to_binary(int(number))}")
+
+main()
